e95126bb23350cbd799f34b6952a319543e8400a
JoshB00 <127239397+JoshB00@users.noreply.github.com>
2023-04-30 22:41:42 -0500

    Added Computer to Tic Tac Toe Program
    Had to Rewrite the entire code to get the computer to work also it would not show my individual changes in Git Extensions for some reason.

--- src/ConsoleApp1/Program.cs
+++ src/ConsoleApp1/Program.cs
@@ -1,147 +1,234 @@
-﻿using System;
-using System.Threading;
-namespace TIC_TAC_TOE
+using System;
+
+namespace TicTacToe
 {
-    class Program
+    class TicTacToeProgram
     {
-        //making array and
-        //by default I am providing 0-9 where no use of zero
-        static char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-        static int player = 1; //By default player 1 is set
-        static int choice; //This holds the choice at which position user want to mark
-        // The flag variable checks who has won if it's value is 1 then someone has won the match
-        //if -1 then Match has Draw if 0 then match is still running
-        static int flag = 0;
         static void Main(string[] args)
         {
-            do
+            char[,] board = new char[3, 3]; // create a 3x3 board
+            int userScore = 0;
+            int computerScore = 0;
+
+
+            //while loop
+            while (true)
             {
-                Console.Clear();// whenever loop will be again start then screen will be clear
-                Console.WriteLine("Player1:X and Player2:O");
-                Console.WriteLine("\n");
-                if (player % 2 == 0)//checking the chance of the player
+                Console.WriteLine("Tic Tac Toe Game");
+                Console.WriteLine($"User Score: {userScore}, Computer Score: {computerScore}\n");
+
+                // initialize board with empty cells
+                for (int i = 0; i < 3; i++)
                 {
-                    Console.WriteLine("Player 2 Chance");
-                }
-                else
-                {
-                    Console.WriteLine("Player 1 Chance");
-                }
-                Console.WriteLine("\n");
-                Board();// calling the board Function
-                choice = int.Parse(Console.ReadLine());//Taking users choice
-                // checking that position where user want to run is marked (with X or O) or not
-                if (arr[choice] != 'X' && arr[choice] != 'O')
-                {
-                    if (player % 2 == 0) //if chance is of player 2 then mark O else mark X
+                    for (int j = 0; j < 3; j++)
                     {
-                        arr[choice] = 'O';
-                        player++;
-                    }
-                    else
-                    {
-                        arr[choice] = 'X';
-                        player++;
+                        board[i, j] = ' ';
                     }
                 }
-                else
-                //If there is any possition where user want to run
-                //and that is already marked then show message and load board again
+
+                int turn = 0;
+                char currentPlayer = 'X';
+
+                while (true)
                 {
-                    Console.WriteLine("Sorry the row {0} is already marked with {1}", choice, arr[choice]);
-                    Console.WriteLine("\n");
-                    Console.WriteLine("Please wait 2 second board is loading again.....");
-                    Thread.Sleep(2000);
+                    MakeBoard(board);
+
+                    if (CheckWinner(board, 'O'))
+                    {
+                        Console.WriteLine("Computer wins!");
+                        computerScore++;
+                        break;
+                    }
+
+                    if (CheckWinner(board, 'X'))
+                    {
+                        Console.WriteLine("You win!");
+                        userScore++;
+                        break;
+                    }
+
+                    if (IsBoardFull(board))
+                    {
+                        Console.WriteLine("Draw!");
+                        break;
+                    }
+
+                    if (turn % 2 == 0) // user's turn
+                    {
+                        Console.Write("Enter row (1-3): ");
+                        int row = int.Parse(Console.ReadLine()) - 1;
+                        Console.Write("Enter column (1-3): ");
+                        int col = int.Parse(Console.ReadLine()) - 1;
+
+                        if (board[row, col] != ' ')
+                        {
+                            Console.WriteLine("That cell is already occupied. Try again.");
+                            continue;
+                        }
+
+                        board[row, col] = currentPlayer;
+                    }
+                    else // computer's turn
+                    {
+                        int[] move = FindBestMove(board);
+                        board[move[0], move[1]] = currentPlayer;
+
+                        Console.WriteLine($"Computer chooses row {move[0] + 1}, column {move[1] + 1}");
+                    }
+
+                    turn++;
+                    currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
                 }
-                flag = CheckWin();// calling of check win
             }
-            while (flag != 1 && flag != -1);
-            // This loop will be run until all cell of the grid is not marked
-            //with X and O or some player is not win
-            Console.Clear();// clearing the console
-            Board();// getting filled board again
-            if (flag == 1)
-            // if flag value is 1 then someone has win or
-            //means who played marked last time which has win
-            {
-                Console.WriteLine("Player {0} has won", (player % 2) + 1);
-            }
-            else// if flag value is -1 the match will be draw and no one is winner
-            {
-                Console.WriteLine("Draw");
-            }
-            Console.ReadLine();
         }
-        // Board method which creats board
-        private static void Board()
+
+        static void MakeBoard(char[,] board)
         {
-            Console.WriteLine("     |     |      ");
-            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[1], arr[2], arr[3]);
-            Console.WriteLine("_____|_____|_____ ");
-            Console.WriteLine("     |     |      ");
-            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[4], arr[5], arr[6]);
-            Console.WriteLine("_____|_____|_____ ");
-            Console.WriteLine("     |     |      ");
-            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[7], arr[8], arr[9]);
-            Console.WriteLine("     |     |      ");
+            Console.WriteLine("   1 2 3");
+            Console.WriteLine("  -------");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write($"{i + 1} |");
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write($"{board[i, j]}|");
+                }
+                Console.WriteLine();
+                Console.WriteLine("  -------");
+            }
+            Console.WriteLine();
         }
-        //Checking that any player has won or not
-        private static int CheckWin()
+        
+        //Check for winner method
+        static bool CheckWinner(char[,] board, char player)
         {
-            #region Horzontal Winning Condtion
-            //Winning Condition For First Row
-            if (arr[1] == arr[2] && arr[2] == arr[3])
+            // check rows
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[i, 0] == player && board[i, 1] == player && board[i, 2] == player)
+                {
+                    return true;
+                }
+            }
+
+            // check columns
+            for (int j = 0; j < 3; j++)
+            {
+                if (board[0, j] == player && board[1, j] == player && board[2, j] == player)
+                {
+                    return true;
+                }
+            }
+
+            // check diagonals
+            if (board[0, 0] == player && board[1, 1] == player && board[0, 0] == player)
+                if (board[2, 0] == player && board[1, 1] == player && board[0, 2] == player)
+                {
+                    return true;
+                }
+
+            return false;
+        }
+
+
+        static bool IsBoardFull(char[,] board)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (board[i, j] == ' ')
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+
+        static int[] FindBestMove(char[,] board)
+        {
+            int bestScore = -1000;
+            int[] move = new int[2];
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (board[i, j] == ' ')
+                    {
+                        board[i, j] = 'O';
+                        int score = Minimax(board, false);
+                        board[i, j] = ' ';
+                        if (score > bestScore)
+                        {
+                            bestScore = score;
+                            move[0] = i;
+                            move[1] = j;
+                        }
+                    }
+                }
+            }
+            return move;
+        }
+
+        static int Minimax(char[,] board, bool isMaximizing)
+        {
+            if (CheckWinner(board, 'O'))
             {
                 return 1;
             }
-            //Winning Condition For Second Row
-            else if (arr[4] == arr[5] && arr[5] == arr[6])
-            {
-                return 1;
-            }
-            //Winning Condition For Third Row
-            else if (arr[6] == arr[7] && arr[7] == arr[8])
-            {
-                return 1;
-            }
-            #endregion
-            #region vertical Winning Condtion
-            //Winning Condition For First Column
-            else if (arr[1] == arr[4] && arr[4] == arr[7])
-            {
-                return 1;
-            }
-            //Winning Condition For Second Column
-            else if (arr[2] == arr[5] && arr[5] == arr[8])
-            {
-                return 1;
-            }
-            //Winning Condition For Third Column
-            else if (arr[3] == arr[6] && arr[6] == arr[9])
-            {
-                return 1;
-            }
-            #endregion
-            #region Diagonal Winning Condition
-            else if (arr[1] == arr[5] && arr[5] == arr[9])
-            {
-                return 1;
-            }
-            else if (arr[3] == arr[5] && arr[5] == arr[7])
-            {
-                return 1;
-            }
-            #endregion
-            #region Checking For Draw
-            // If all the cells or values filled with X or O then any player has won the match
-            else if (arr[1] != '1' && arr[2] != '2' && arr[3] != '3' && arr[4] != '4' && arr[5] != '5' && arr[6] != '6' && arr[7] != '7' && arr[8] != '8' && arr[9] != '9')
+            if (CheckWinner(board, 'X'))
             {
                 return -1;
             }
-            #endregion
-            else
+            if (IsBoardFull(board))
             {
                 return 0;
+            }
+
+            if (isMaximizing)
+            {
+                int bestScore = -1000;
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if (board[i, j] == ' ')
+                        {
+                            board[i, j] = 'O';
+                            int score = Minimax(board, false);
+                            board[i, j] = ' ';
+                            if (score > bestScore)
+                            {
+                                bestScore = score;
+                            }
+                        }
+                    }
+                }
+                return bestScore;
+            }
+            else
+            {
+                int bestScore = 1000;
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if (board[i, j] == ' ')
+                        {
+                            board[i, j] = 'X';
+                            int score = Minimax(board, true);
+                            board[i, j] = ' ';
+                            if (score < bestScore)
+                            {
+                                bestScore = score;
+                            }
+                        }
+                    }
+                }
+                return bestScore;
             }
         }
     }
